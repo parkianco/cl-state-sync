79e3b3228b658818f3eb611c9db5ed62b21a1be6
Park Ian Co <opensource@parkian.co>
2026-03-11 19:50:30 -0400

    Fix copyright attribution and add headers

--- src/reconstruct.lisp
+++ src/reconstruct.lisp
@@ -1,3 +1,6 @@
+;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
 ;;;; cl-state-sync - State Reconstruction
 ;;;;
 ;;;; Reconstructs UTXO set and state trie from snapshot chunks.
--- src/snapshot.lisp
+++ src/snapshot.lisp
@@ -1,3 +1,6 @@
+;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
 ;;;; cl-state-sync - Snapshot Format
 ;;;;
 ;;;; Defines the snapshot format for UTXO set and state serialization.
